9326d733fd2662adb28bc352bebf542490a53aa9
MFaik <m.faikekclr@gmail.com>
2024-12-26 03:35:43 +0300

    hafta 2 merge-sort improvements

--- hafta-2/merge-sort.py
+++ hafta-2/merge-sort.py
@@ -1,7 +1,42 @@
-def merge_sort(arr):
-    q = [0]*len(arr)
-    def merge_sorted(l, m, r):
+# returns the sorted array arr
+def merge_sorted(arr):
+    def rev_sort(arr):
+        if len(arr) < 2:
+            return arr
+
+        m = len(arr)//2
+        left = rev_sort(arr[:m])
+        right = rev_sort(arr[m:])
+
+        ret = []
+        while len(left) > 0:
+            if len(right) > 0 and right[-1] < left[-1]:
+                ret.append(right[-1])
+                right.pop()
+            else:
+                ret.append(left[-1])
+                left.pop()
+        while len(right) > 0:
+            ret.append(right[-1])
+            right.pop()
+        ret.reverse()
+        return ret
+
+    ret = rev_sort(arr)
+    ret.reverse()
+    return ret
+
+# sorts the array in place, but still uses O(n) space
+def merge_sort_inplace(arr):
+    # q is a queue
+    # ql points to the first number in queue
+    # qr points to the number after the last number
+    # [ql, qr) is the range of q
+    q = [0]*(len(arr)//2)
+    # merges the ranges [l, m) and [m, r) in the array
+    def merge(l, m, r):
         ql, qr = 0, 0
+
         for _ in range(m-l):
             if arr[m] < arr[l] and (not ql < qr or arr[m] < q[ql]):
                 q[qr], arr[l] = arr[l], arr[m] 
@@ -22,13 +57,14 @@ def merge_sort(arr):
                 ql += 1
             l += 1
             
-    def merge_part(l, r):
+    def sort(l, r):
         if l < r-1:
-            merge_part(l, (r-l)//2+l)
-            merge_part((r-l)//2+l, r)
-            merge_sorted(l, (r-l)//2+l, r)
+            m = (r-l)//2+l
+            sort(l, m)
+            sort(m, r)
+            merge(l, m, r)
     
-    merge_part(0, len(arr))
+    sort(0, len(arr))
 
 if __name__ == "__main__":
     from random import randint
@@ -44,13 +80,23 @@ if __name__ == "__main__":
         arr = [randint(1, 100000) for _ in range(arr_len)]
         arr_copy = [x for x in arr]
 
-        merge_sort(arr)
+        merge_sort_inplace(arr)
         if not is_sorted(arr):
             h = {}
             for i, n in enumerate(sorted(arr_copy)):
                 h[n] = i
             arr_copy = [h[x] for x in arr_copy]
             arr = [h[x] for x in arr]
-            print("tried to sort ", arr_copy)
+            print("tried to merge-sort-inplace ", arr_copy)
+            print("but got ", arr)
+            assert(False)
+
+        if not is_sorted(merge_sorted(arr)):
+            h = {}
+            for i, n in enumerate(sorted(arr_copy)):
+                h[n] = i
+            arr_copy = [h[x] for x in arr_copy]
+            arr = [h[x] for x in arr]
+            print("tried to merge-sort-inplace ", arr_copy)
             print("but got ", arr)
             assert(False)
